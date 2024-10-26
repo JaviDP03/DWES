ca3e7658d7368dcceefd0c986d507ea167646e54
Javier Duarte <javiduarte34@gmail.com>
2024-10-26 15:04:18 +0200

    Tabla hecha

--- UT3/BD/actividad35.php
+++ UT3/BD/actividad35.php
@@ -5,26 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actividad 3.5</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 
 <body>
     <h2>Datos de un producto</h2>
     <form>
         <p>Código: <input type="text" name="codigo"></p>
-        <input type="submit" name="submit">
+        <p><input type="submit" name="submit"></p>
     </form>
 
     <?php
     $codigo = $_GET['codigo'];
 
     $bd = new PDO("mysql:host=localhost;dbname=tienda", "root", "");
-    $resultado = $bd->query("SELECT * FROM producto WHERE cod = '$codigo'");
-    while ($registro = $resultado->fetch()) {
-        echo "<pre>";
-        print_r($registro);
-        echo "</pre>";
-    }
-    ?>
+    $resultado = $bd->query("SELECT * FROM producto WHERE cod = '$codigo'"); ?>
+    <table>
+        <tr>
+            <th>Código</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Precio</th>
+            <th>Familia</th>
+        </tr>
+        <?php
+        while ($registro = $resultado->fetch()) {
+            echo "<tr>";
+            echo "<td>" . $registro['cod'] . "</td>";
+            echo "<td>" . $registro['nombre_corto'] . "</td>";
+            echo "<td>" . $registro['descripcion'] . "</td>";
+            echo "<td>" . $registro['PVP'] . "</td>";
+            echo "<td>" . $registro['familia'] . "</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
 </body>
 
 </html>
